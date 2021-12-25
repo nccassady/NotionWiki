7c400f926a373d7ca7d5954bb88b2e21e9a74a55
Nick Cassady <nccassady@gmail.com>
2021-12-24 18:06:45 -0800

    Switch to pytest

--- tests/test_notion.py
+++ tests/test_notion.py
@@ -1,26 +1,18 @@
-import unittest
-
-import config
+from .. import config
 import notion_client
-from utils.Notion import Notion
+from ..utils.Notion import Notion
 
 
-class TestNotion(unittest.TestCase):
-    @classmethod
-    def setUpClass(self):
-        self.client = Notion(config.API_SECRET, config.DATABASE_ID)
+class TestNotion:
+    client = Notion(config.API_SECRET, config.DATABASE_ID)
 
     def test_new_instance(self):
-        self.assertIsInstance(self.client.client, notion_client.Client)
-        self.assertIn("properties", self.client.database)
-        self.assertGreater(len(self.client.properties), 0)
+        assert isinstance(self.client.client, notion_client.Client)
+        assert "properties" in self.client.database
+        assert len(self.client.properties) > 0
 
     def test_update_column_check(self):
-        self.assertNotEqual(False, self.client.checkForUpdateColumn())
+        assert self.client.checkForUpdateColumn()
 
     def test_get_wikidata_ids(self):
-        self.assertGreater(len(self.client.getWikiDataIds()), 0)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        assert len(self.client.getWikiDataIds()) > 0
--- tests/test_wiki.py
+++ tests/test_wiki.py
@@ -1,22 +1,14 @@
-import unittest
-
 import wptools
-from utils.Wiki import Wiki
+from ..utils.Wiki import Wiki
 
 
-class TestWiki(unittest.TestCase):
-    @classmethod
-    def setUpClass(self):
-        self.result = Wiki("Hericium erinaceus")
+class TestWiki:
+    result = Wiki("Hericium erinaceus")
 
     def test_new_instance(self):
-        self.assertIsInstance(self.result.page, wptools.page)
+        assert isinstance(self.result.page, wptools.page)
 
     def test_load_data(self):
-        self.result.load_data()
-        self.assertIn("requests", self.result.page.data)
-        self.assertIn("wikidata", self.result.page.data["requests"])
-
-
-if __name__ == "__main__":
-    unittest.main()
+        self.result.getData()
+        assert "requests" in self.result.page.data
+        assert "wikidata" in self.result.page.data["requests"]
